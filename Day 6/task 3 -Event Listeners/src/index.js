776528d79fd6a6f491496a980f25a47518f14f06
Ganesh S <79785517+ganesh-exe@users.noreply.github.com>
2022-02-01 15:40:59 +0530

    commit 6

---
+++ Day 6/task 3 -Event Listeners/src/index.js
@@ -0,0 +1,41 @@
+import "./styles.css";
+var par = document.getElementById("app");
+var btn1 = document.getElementById("btn1");
+var btn2 = document.getElementById("btn2");
+var btn3 = document.getElementById("btn3");
+
+btn1.addEventListener("click", function () {
+  par.classList.add("class1");
+});
+btn1.addEventListener("mouseover", function () {
+  var msg1 = document.createElement("p");
+  msg1.textContent = "Your mouse is on button1";
+  par.appendChild(msg1);
+});
+btn1.addEventListener("mouseout", function () {
+  par.removeChild(par.lastChild);
+});
+
+btn2.addEventListener("click", function () {
+  par.classList.add("class2");
+});
+btn2.addEventListener("mouseover", function () {
+  var msg2 = document.createElement("p");
+  msg2.textContent = "Your mouse is on button2";
+  par.appendChild(msg2);
+});
+btn2.addEventListener("mouseout", function () {
+  par.removeChild(par.lastChild);
+});
+
+btn3.addEventListener("click", function () {
+  par.classList.add("class3");
+});
+btn3.addEventListener("mouseover", function () {
+  var msg3 = document.createElement("p");
+  msg3.textContent = "Your mouse is on button3";
+  par.appendChild(msg3);
+});
+btn3.addEventListener("mouseout", function () {
+  par.removeChild(par.lastChild);
+});
