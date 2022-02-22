48cef5a4d96a9020b0715615ae40f9067d646b01
Ganesh S <79785517+ganesh-exe@users.noreply.github.com>
2022-02-22 17:36:43 +0530

    day 21 - 22/02/22

---
+++ React JS training (Day 16 - 21)/Day 21 (22-02-22)/task 2/my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TotalCalories from './components/TotalCalories';
+
+function App() {
+  return (
+    <div className="App">
+     <TotalCalories/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React JS training (Day 16 - 21)/Day 21 (22-02-22)/task 2/my-app/src/components/TotalCalories.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TotalCalories = () => {
+  return (
+    <div>TotalCalories</div>
+  )
+}
+
+export default TotalCalories
